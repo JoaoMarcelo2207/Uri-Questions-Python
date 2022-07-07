a04f63042f1835c2688f34e583563223ded25ba9
João Marcelo <jmcostamiranda@hotmail.com>
2022-07-07 19:47:34 -0300

    adicionou os arquivos

---
+++ 1008.py
@@ -0,0 +1,8 @@
+num = int(input())
+qnt = int(input())
+salh = float(input())
+
+sal = float(salh*qnt)
+
+print('NUMBER =', num)
+print('SALARY = U$ {:.2f}'.format(sal))
---
+++ 1009.py
@@ -0,0 +1,6 @@
+nome = str(input())
+salf = float(input())
+montante = float(input())
+
+salt = salf + (0.15*montante)
+print('TOTAL = R$ {:.2f}'.format(salt))
