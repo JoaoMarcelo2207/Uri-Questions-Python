214b7bb88ac851759f6f8004ef7ba74cf3948047
João Marcelo <jmcostamiranda@hotmail.com>
2022-07-08 12:42:28 -0300

    adicionou arquivos

---
+++ 1050.py
@@ -0,0 +1,19 @@
+var = int(input())
+
+if var == 61:
+    print('Brasilia')
+elif var == 71:
+    print('Salvador')
+elif var == 11:
+    print('Sao Paulo')
+elif var == 21:
+    print('Rio de Janeiro')
+elif var == 32:
+    print('Juiz de Fora')
+elif var == 19:
+    print('Campinas')
+elif var == 27:
+    print('Vitoria')
+elif var == 31:
+    print('Belo Horizonte')
+else: print('DDD nao cadastrado')
